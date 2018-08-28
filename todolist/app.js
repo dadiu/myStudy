878bcd9a15e624a1fa83cb243871a25b7ba717da
whj <1004609378@qq.com>
2018-08-28 11:21:44 +0800

    完善练习

--- todolist/app.js
+++ todolist/app.js
@@ -1,9 +1,15 @@
+/*
+ * @Author: whj 
+ * @Date: 2018-08-28 11:07:20 
+ * @Last Modified by: whj
+ * @Last Modified time: 2018-08-28 11:07:49
+ */
+
 var express = require('express');
 
 var todoController = require('./controllers/todoController');
 
 var app = express();
-
 
 app.set('view engine', 'ejs');
 
--- todolist/controllers/todoController.js
+++ todolist/controllers/todoController.js
@@ -2,20 +2,23 @@
  * @Author: whj 
  * @Date: 2018-08-27 16:59:24 
  * @Last Modified by: whj
- * @Last Modified time: 2018-08-27 17:38:07
+ * @Last Modified time: 2018-08-28 11:07:12
  */
-
-var data = [
-    { item: 'get milk' },
-    { item: 'walk dog' },
-    { item: 'get flowers' }
-]
 
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
-const mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://dadiu:dadiu2018@ds117839.mlab.com:17839/daywork');
+mongoose.connect(
+    'mongodb://dadiu:dadiu2018@ds117839.mlab.com:17839/daywork', { useNewUrlParser: true },
+    function(err) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log('success')
+        }
+    }
+);
 
 var todoSchema = new mongoose.Schema({
     item: String
@@ -23,25 +26,26 @@ var todoSchema = new mongoose.Schema({
 
 var Todo = mongoose.model('Todo', todoSchema);
 
-var itemOne = Todo({ item: 'buy flower' }).save(function(err) {
-    if (err) throw err;
-    console.log('item saved');
-})
-
 module.exports = function(app) {
     app.get('/todo', function(req, res) {
-        res.render('todo', { todos: data });
+        Todo.find({}, function(err, data) {
+            if (err) throw err;
+            res.render('todo', { todos: data })
+        })
     });
 
     app.post('/todo', urlencodedParser, function(req, res) {
-        data.push(req.body);
-        res.json(data);
+
+        Todo(req.body).save(function(err, data) {
+            if (err) throw err;
+            res.json(data);
+        });
     });
 
     app.delete('/todo/:item', function(req, res) {
-        data = data.filter(function(todo) {
-            return todo.item.replace(/ /g, '-') !== req.params.item;
+        Todo.find({ item: req.params.item.replace(/-/g, ' ') }).remove(function(err, data) {
+            if (err) throw err;
+            res.json(data);
         })
-        res.json(data);
     })
 }
